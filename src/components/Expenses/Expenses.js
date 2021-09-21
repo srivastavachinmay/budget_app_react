bdb82546a1d552db3f600d92a3f2348aab472d4b
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2021-09-21 07:44:24 +0530

    react state and working with events x

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import './Expenses.css'
+import ExpenseItem       from "./ExpenseItem";
+import Card              from '../UI/Card'
+import ExpensesFilter    from "./ExpensesFilter";
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2021')
+
+    const filterChangeHandler = selectedYear => {
+        console.log('Expenses.js')
+        setFilteredYear(selectedYear)
+    }
+    return (
+        <div>
+            <Card className={"expenses"}>
+                <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                />
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}
+                />
+                <ExpenseItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}
+                />
+            </Card>
+        </div>
+    );
+}
+export default Expenses
